d27672200c2d9beaea01695d428e60ffc09129c9
NishatFariza <nishatfarzia78@gmail.com>
2022-10-18 23:13:53 +0600

    refector: env variable name

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -4,8 +4,8 @@ import GithubProvider from "next-auth/providers/github"
 export default NextAuth({
   providers: [
      GithubProvider({
-      clientId: process.env.GITHUB_CLIENT_ID as string,
-      clientSecret: process.env.GITHUB_SECRET as string,
+      clientId: process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID as string,
+      clientSecret: process.env.NEXT_PUBLIC_GITHUB_SECRET as string,
     }),
   ]
 })
