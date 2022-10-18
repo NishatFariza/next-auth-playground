f3fe0fc3cd7440ab12bf80c61e96445ed96ab93f
NishatFariza <nishatfarzia78@gmail.com>
2022-10-18 11:12:46 +0600

    wip:loginPage

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,13 +5,13 @@ const Navbar = () => {
   return (
     <div className=" max-w-6xl mx-auto flex justify-between items-center h-20">
       <Link href={`/`}>
-        <a>NextAuth</a>
-          </Link>
-          
-          <nav className="flex ">
-              <p>Nishat</p>
-              <button>Logout</button>
-          </nav>
+        <a className="text-2xl font-semibold">NextAuth</a>
+      </Link>
+
+      <nav className="flex items-center gap-2">
+        <p>Nishat</p>
+        <button className="bg-gray-200 px-4 rounded">Logout</button>
+      </nav>
     </div>
   );
 };
---
+++ src/pages/auth/login.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const loginPage = () => {
+  return <div>loginPage</div>;
+};
+
+export default loginPage;
