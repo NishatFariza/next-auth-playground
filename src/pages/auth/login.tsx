c447d6219c3949910c42a14d48e25230799d33f6
NishatFariza <nishatfarzia78@gmail.com>
2022-10-18 11:35:27 +0600

    wip:login form

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -1,7 +1,25 @@
 import React from "react";
 
 const loginPage = () => {
-  return <div>loginPage</div>;
+  return (
+    <div className="max-w-6xl mx-auto">
+      <div className="w-6/12 mx-auto">
+        <h1 className="text-2xl font-semibold text-center">Login to your account</h1>
+
+        <div className="flex flex-col gap-3 mt-8">
+          <button className="text-lg border rounded py-2 px-4">
+            Log in with Google
+          </button>
+          <button className="text-lg border rounded py-2 px-4">
+            Log in with GitHub
+          </button>
+          <button className="text-lg border rounded py-2 px-4">
+            Log in with Twitter
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default loginPage;
